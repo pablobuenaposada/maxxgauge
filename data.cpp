14587bd0586358a65128cc1030443c8f82421f41
unknown <pablo.buenaposada@gmail.com>
2024-09-08 17:23:37 +0200

    add dct temp

--- data.cpp
+++ data.cpp
@@ -18,7 +18,7 @@ Page pages[] = {{"GEAR", 150, 50, 0x536, 0, 2, 1, true, "%d", false, NULL, NULL,
                 {"BAT", 80, 80, 0x530, 0, 2, 0.01, false, "%.1f", false, NULL, NULL, {}},
                 {"TPS", 80, 80, 0x520, 2, 2, 0.1, true, "%d", false, NULL, NULL, {}},
                 {"COOLANT", 80, 80, 0x530, 6, 2, 0.1, true, "%d", false, NULL, NULL, {}},
-                {"RPM", 60, 90, 0x520, 0, 2, 1, true, "%d", false, NULL, NULL, {}},
+                {"DCT TEMP", 80, 80, 0x540, 4, 4, 0.1, true, "%d", false, NULL, NULL, {}},
                 {"DCT MODE", 80, 80, 0x107, 2, 2, 1, true, "%d", true, 0x100, 0x101, {}},
                 {"DRIVELOGIC", 80, 80, 0x107, 0, 2, 1, true, "%d", true, 0x102, 0x103, {}}};
 
